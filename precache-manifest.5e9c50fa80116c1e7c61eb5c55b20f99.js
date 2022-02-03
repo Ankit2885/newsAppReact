b3997a910c18d110cabb66d64b63bba180e6cedb
Ankit2885 <ak176140@gmail.com>
2022-02-02 20:01:54 -0800

    Updates

---
+++ precache-manifest.5e9c50fa80116c1e7c61eb5c55b20f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2d513e726d6b1376f4d7092cfdf668",
+    "url": "/newsAppReact/index.html"
+  },
+  {
+    "revision": "3147774a544b833c239f",
+    "url": "/newsAppReact/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "de90adc7678815f08a6d",
+    "url": "/newsAppReact/static/js/2.8cb6736b.chunk.js"
+  },
+  {
+    "revision": "d14e4b2bc92b6ae455e9",
+    "url": "/newsAppReact/static/js/3.4ea34601.chunk.js"
+  },
+  {
+    "revision": "3147774a544b833c239f",
+    "url": "/newsAppReact/static/js/main.9f5979f9.chunk.js"
+  },
+  {
+    "revision": "897bc535d35a8d92e350",
+    "url": "/newsAppReact/static/js/runtime~main.66283af2.js"
+  }
+]);
